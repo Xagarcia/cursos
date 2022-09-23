721c8d2315f53bc52615a1863356a73ade241047
Xagarcia <113516927+Xagarcia@users.noreply.github.com>
2022-09-23 14:07:22 +0200

    Add files via upload

---
+++ libft/ft_strrchr.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strrchr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: xagarcia <xagarcia@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/23 12:57:35 by xagarcia          #+#    #+#             */
+/*   Updated: 2022/09/23 13:04:58 by xagarcia         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+const	char* ft_strrchr( const char* str, int c)
+{
+	int	strlen;
+
+	strlen = ft_strlen(str);
+	while (strlen >= 0)
+	{
+		if (str[strlen] == c)
+		{
+			return ((char*)&str[strlen]);
+		}
+		strlen--;
+	}
+	return (NULL);
+}
